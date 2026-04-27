296c742ad883884ae7098d85194f962d3a07efa0
Spandana Chittaluri <spandana.chittaluri@gmail.com>
2026-04-27 10:16:14 -0500

    your own message describing your change

--- LessonA/EvenOdd.java
+++ LessonA/EvenOdd.java
@@ -2,11 +2,11 @@ package LessonA;
 
 public class EvenOdd {
   public static void main(String[] args) {
-    int[] numbers = {3,8,12,7,5,16,21,4,19,2};
+    int[] inputNumbers = {3,8,12,7,5,16,21,4,19,2};
     int evenCount =0;
     int oddCount =0;
-     for (int i=0; i<numbers.length; i++) {
-        if(numbers[i] % 2 == 0){
+     for (int i=0; i<inputNumbers.length; i++) {
+        if(inputNumbers[i] % 2 == 0){
           evenCount++;
         }
         else {
