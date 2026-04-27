03a9182ccd7e005e472f848da3325cee64c348bc
Spandana Chittaluri <spandana.chittaluri@gmail.com>
2026-04-27 09:38:12 -0500

    Initial commit: even/odd counter program

---
+++ LessonA/EvenOdd.java
@@ -0,0 +1,19 @@
+package LessonA;
+
+public class EvenOdd {
+  public static void main(String[] args) {
+    int[] numbers = {3,8,12,7,5,16,21,4,19,2};
+    int evenCount =0;
+    int oddCount =0;
+     for (int i=0; i<numbers.length; i++) {
+        if(numbers[i] % 2 == 0){
+          evenCount++;
+        }
+        else {
+          oddCount++;
+     }
+     }
+     System.out.println("Even count: "+evenCount);
+     System.out.println("Odd count: "+oddCount);
+  }
+}
